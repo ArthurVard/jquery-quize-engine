be8af35c5b7f73f455cc570ebaa883f56fc25146
Arthur Vardanyan <artie.vard@gmail.com>
2014-05-21 12:46:58 +0400

    init commit

---
+++ init.js
@@ -0,0 +1,87 @@
+﻿
+ var init = { 
+     'questions': [ 
+       {
+           'question': 'Ո՞ր դեպքերում առաջին դասարանի սովորողը կարող է կրկնել տվյալ ուսումնական տարվա դասընթացը:',
+           'answers': ['Ուսումնական հաստատության տնօրենի ու դասվարի որոշմամբ և ծնողի (նրա օրինական ներկայացուցչի համաձայնությամբ',
+           'Ծնողի (նրա օրինական ներկայացուցչի) գրավոր դիմումի և Հաստատության մանկավարժական խորհրդի  պահանջով',
+           'Ծնողի (նրա օրինական ներկայացուցչի) գրավոր դիմումի և Հաստատության մանկավարժական խորհրդի որոշմամբ:',
+           "Ծնողի (նրա օրինական ներկայացուցչի) խնդրանքով և Հաստատության մանկավարժական խորհրդի որոշմամբ:"],
+			  'correctAnswer': 3
+       },
+        {
+           'question': 'The C in CSS stands for?',
+           'answers': ['Controlled','Color','Cascading','Consistent'],
+			  'correctAnswer': 3
+       },
+
+       {
+           'question': 'What is the correct HTML for creating a hyperlink?',
+           'answers': ['&lt;a name="http://www.w3schools.com">W3Schools.com&lt;/a>','&lt;a href="http://www.w3schools.com">W3Schools&lt;/a>','&lt;a url="http://www.w3schools.com">W3Schools.com</a>','&lt;a>http://www.w3schools.com&lt;/a>'],
+			  'correctAnswer': 2
+       },
+
+		{
+           'question': 'What is the total width of this container? (think box-model) <br>#container {width: 100px; padding: 20px 10px;}',
+           'answers': ['100','110','120', '140px'],
+			  'correctAnswer': 3
+       },
+       {
+           'question': 'Which of the following is used to display links without an underline?',
+           'answers': ['{text-decoration:no underline}','{text-decoration:none}','{font-style:underlined 0}','{hyperlink:no underline}'],
+			  'correctAnswer': 2
+       },
+               {
+           'question': 'What is the correct HTML tag for the most important heading?',
+           'answers': ['head','h6','largehead','h1'],
+			  'correctAnswer': 4
+       },
+
+
+       {
+           'question': 'How do you center an element horizontally?',
+           'answers': ['margin: 0 auto;','align: center;','center: true;','float: 50%'],
+			  'correctAnswer': 1
+       },
+
+
+       {
+           'question': 'Which element is an unordered list?',
+           'answers': ['&lt;list>','&lt;oul>','&lt;ol>','&lt;ul>'],
+			  'correctAnswer': 4	
+       },
+
+       {
+           'question': 'The # symbol specifies that the selector is...',
+           'answers': ['class','element','tag','id'],
+			  'correctAnswer': 4
+       },
+      {
+           'question': 'What is the correct HTML for inserting an image?',
+           'answers': ['&lt;img href="image.gif" alt="MyImage" />','&lt;img alt="MyImage">image.gif</img>',	'&lt;image src="image.gif" alt="MyImage" />','&lt;img src="image.gif" alt="MyImage" />'],
+			  'correctAnswer': 4
+       },
+       {
+           'question': '<code>#id {margin: 20px 30px 10px 50px}</code><br><br> The margin-right value will be?',
+           'answers': ['10px','20px','50px','30px'],
+			  'correctAnswer': 4
+       },
+       {
+           'question': 'Which property is used to change the size of text?',
+           'answers': ['size','type','font-size','type-size'],
+			  'correctAnswer': 3
+       }
+
+     ],
+	  'resultComments' :  
+	  {
+		    perfect: 'Wow. Perfect!',
+			 excellent: 'Outstanding!',
+			 good: 'Exceeds expectations!',
+			 average: 'Acceptable.',
+			 bad: 'Keep at it!',
+			 poor: 'Keep at it!',
+			 worst: 'Keep at it!'
+	  }
+
+ };
